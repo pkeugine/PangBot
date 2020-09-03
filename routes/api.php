620509c2279eb9289b4d33fc87666e61fbb769e7
Eugine Park <pkeugine@gmail.com>
2020-09-03 23:58:13 +0900

    add testing api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/testing', function() {
+    return ['message' => 'hello there!'];
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
